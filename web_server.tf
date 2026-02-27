34aa13df086a7c494b1f77426074a1bba5f58529
daniel09077 <danielokuguni15@gmail.com>
2026-02-27 15:51:22 +0100

    user data updated

--- web_server.tf
+++ web_server.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "web_servers" {
   count           = 2 # Create TWO servers
   ami             = var.ami
   instance_type   = var.instance_type
-  security_groups = [aws_security_group.alb_sg.id]
+  security_groups = [aws_security_group.web_sg.id]
+
   # This picks subnet [0] for the first server and [1] for the second
   subnet_id            = values(local.app_subnet_ids)[count.index]
   iam_instance_profile = aws_iam_instance_profile.php_ssm_profile.name
